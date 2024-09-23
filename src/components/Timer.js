afb2df6c594caa6c7fa0b57bb4156f89bffb500c
RalphHalabi <ralphhalabi22@gmail.com>
2024-09-23 15:00:47 +0300

    unused

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,7 +6,7 @@ export default function Timer({ dispatch, secondsRemaining }) {
 
   useEffect(() => {
     const id = setInterval(function () {
-      dispatch({ type: "tick", payload: "" });
+      dispatch({ type: "tick" });
     }, 1000);
 
     return () => clearInterval(id);
